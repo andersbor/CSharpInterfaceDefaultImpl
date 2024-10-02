07020160e573d960428505d2bf54d4c7237ba784
andersbor <anbo@zealand.dk>
2024-10-02 19:59:50 +0200

    Add project files.

---
+++ InterfaceDefaultImpl/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+using InterfaceDefaultImpl;
+
+Console.WriteLine("Hello, World!");
+
+SomeInterface obj = new SomeInterfaceImpl();
+obj.Method1();
+obj.Method2();
+obj.Method3();
+
+SomeInterfaceImpl obj2 = new SomeInterfaceImpl();
+//obj2.Method3(); // Does not compile!!
+
+AnotherInterface obj3 = new SomeInterfaceImpl();
+obj3.Method1();
+obj3.Method2();
+obj3.Method3();
---
+++ InterfaceDefaultImpl/SomeInterface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDefaultImpl
+{
+    public interface SomeInterface
+    {
+        void Method1();
+        void Method2();
+        void Method3() { Console.WriteLine("Some Interface Method3"); }
+    }
+
+    public interface AnotherInterface
+    {
+        void Method1();
+        void Method2();
+        void Method3() { Console.WriteLine("Another Interface Method3"); }
+    }
+
+    class SomeInterfaceImpl : SomeInterface, AnotherInterface
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Method2");
+        }
+
+
+    }
+}
+
+
